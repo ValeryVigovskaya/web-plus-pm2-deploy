30b043d10a30c4b27d6a7f5a6212aa14366fca71
Valeria Vigovskaya <vigovskaya.lera@yandex.ru>
2024-03-18 23:47:21 +0300

    созданы конфиги и запущена pm2

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'frontend',
+    script: 'src/index.js',
+  }],
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/ValeryVigovskaya/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./build ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH} && npm run build && scp -Cr ./build/* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd frontend && npm i && npm run build',
+    },
+  },
+};

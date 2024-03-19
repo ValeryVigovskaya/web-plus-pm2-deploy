d84251cfc54f350cbe353da21f9db44be99d298f
Valeria Vigovskaya <vigovskaya.lera@yandex.ru>
2024-03-19 21:27:52 +0300

    test

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       repo: 'https://github.com/ValeryVigovskaya/web-plus-pm2-deploy.git',
       cwd: 'backend',
       path: DEPLOY_PATH,
-      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST}${DEPLOY_PATH}`,
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
       // 'pre-deploy': 'pm2 startOrRestart ecosystem.json --env production',
       'post-deploy': 'npm i && npm run start && pm2 startOrRestart ecosystem.json --env production',
     },

cb5d1fc207d96c46cc8b8a47ad35489c8979ba12
Valeria Vigovskaya <vigovskaya.lera@yandex.ru>
2024-03-19 13:14:20 +0300

    перенастроен конфиг фронтенда

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       repo: 'https://github.com/ValeryVigovskaya/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
       'pre-deploy': `scp scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH} && scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && npm i && npm run build',
+      'post-deploy': 'npm i && npm run start',
     },
   },
 };

fcb64a743e1ca1171337fbe4afdbf6a8cf4c2f99
Valeria Vigovskaya <vigovskaya.lera@yandex.ru>
2024-03-20 21:42:52 +0300

    test

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -5,10 +5,10 @@ const {
 } = process.env;
 
 module.exports = {
-  // apps: [{
-  //   name: 'mesto-frontend',
-  //   script: './src/index.js',
-  // }],
+  apps: [{
+    name: 'mesto-frontend',
+    script: './src/index.js',
+  }],
   // Настройка деплоя
   deploy: {
     production: {
@@ -18,7 +18,7 @@ module.exports = {
       repo: 'https://github.com/ValeryVigovskaya/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
       'pre-deploy-local': `npm run build && scp -Cr ./build/* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'cd frontend && npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'cd frontend && npm install && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };

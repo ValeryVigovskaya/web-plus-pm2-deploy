194682ccfd2d0b92463e97c4f974a61eb1d4732a
Valeria Vigovskaya <vigovskaya.lera@yandex.ru>
2024-03-20 22:51:38 +0300

    test

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -17,8 +17,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/ValeryVigovskaya/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      // 'pre-deploy-local': `npm run build && scp -Cr ./build/* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'pre-deploy-local': `npm run build && scp -Cr ./build/* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/frontend`,
+      'post-deploy': 'cd frontend && npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
